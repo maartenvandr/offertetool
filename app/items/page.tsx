5ad2821cf6b97303c224d019857d0355fdac8857
maartenvandr <maartenvan11@gmail.com>
2026-01-13 21:18:04 +0100

    Update page.tsx

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useMemo, useState } from "react";
-import { supabase } from "@/lib/supabaseClient";
+import { supabase } from "../../lib/supabaseClient";
 
 type Item = { id: number; name: string; unit_price: number };
 
